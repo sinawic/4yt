47f6abb4db5be7116c01ee1612399b9276b1d36b
sinawic <sinawic9@gmail.com>
2022-03-24 01:24:50 +0430

    scp added to mongo backup bash script

--- scripts/backup-mongodb.sh
+++ scripts/backup-mongodb.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 export CONTAINER_NAME="mongo_mongo-test_1"
-export BACKUP_LOCATION="/home/sinawic/scripts/mongo-backups"
+export BACKUP_LOCATION="/home/sinawic/4yt/scripts/mongo-backups"
 export DATABASE_NAME="test"
 
 export TIMESTAMP=$(date +'%Y%m%d%H%M%S')
 
 docker exec -t ${CONTAINER_NAME} mongodump --authenticationDatabase admin --username root --password example --out /data/backup-${TIMESTAMP}
 docker cp ${CONTAINER_NAME}:/data/backup-${TIMESTAMP} ${BACKUP_LOCATION}
+
+cd ${BACKUP_LOCATION}
+echo zipping...
+zip -r backup-${TIMESTAMP}.zip backup-${TIMESTAMP}
+echo scp...
+scp -P 2124 `pwd`/backup-${TIMESTAMP}.zip root@95.80.157.17:/root/back
