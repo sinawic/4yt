17957a199ebd24474d502c13fb267a6093ea8783
sinawic <sina_041_sm@yahoo.com>
2022-11-04 12:17:18 +0330

    dockerize pure php files added

---
+++ dockerize/apache-php/app/app.php
@@ -0,0 +1,2 @@
+<?php
+  echo 'echo from app';
---
+++ dockerize/apache-php/app/index.php
@@ -0,0 +1,5 @@
+<?php 
+    include('./app.php');
+    echo '<h1>Hello World!</h1>'.$_SERVER['SERVER_ADDR'].':'.$_SERVER['SERVER_PORT'];
+    phpinfo();
+?> 
