509c4053b15925fce721cfcfbc9f652a0d31010a
sinawic <sina_041_sm@yahoo.com>
2023-01-07 15:00:40 +0330

    remote development env `vscode` & `react` in docker

---
+++ vscode/vscode-inbrowser-dev/code/src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <p>
+          Edit <code>hello vscode</code> and save to reload.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
