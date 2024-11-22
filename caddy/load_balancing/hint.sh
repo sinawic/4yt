c841e881d113cf747ce9c298780a906006559765
sinawic <sina_041_sm@yahoo.com>
2024-11-22 23:40:48 +0330

    load_balancing cors added

---
+++ caddy/load_balancing/hint.sh
@@ -0,0 +1,5 @@
+
+# https://caddyserver.com/docs/caddyfile-tutorial
+# https://caddyserver.com/docs/caddyfile/directives/reverse_proxy
+
+mkdir config data
---
+++ caddy/load_balancing/readme.md
@@ -0,0 +1,4 @@
+
+
+# Caddy load balancing
+
