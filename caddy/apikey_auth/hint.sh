5a07f982c1f786d1d6f656839a391b493e73c11e
sinawic <sinawic9@gmail.com>
2024-12-19 22:34:11 +0330

    added caddy apikey_auth

---
+++ caddy/apikey_auth/hint.sh
@@ -0,0 +1,8 @@
+
+# https://caddyserver.com/docs/caddyfile-tutorial
+
+mkdir config data
+
+curl -i http://localhost -H "Authorization: Bearer key1"
+curl -i http://localhost -H "Authorization: Bearer key2"
+curl -i http://localhost -H "Authorization: Bearer key3"
